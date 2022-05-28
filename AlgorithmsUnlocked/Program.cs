545bbedebb2e4f37594185e67c58aa3fe64d736c
hazadus <hazadus7@gmail.com>
2022-05-28 20:19:34 +0300

    Создан скелет программы

---
+++ AlgorithmsUnlocked/Program.cs
@@ -0,0 +1,14 @@
+﻿//
+// "Algorithms Unlocked" book
+//
+
+namespace AlgorithmsUnlocked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithms Unlocked.");
+        }
+    }   
+}
